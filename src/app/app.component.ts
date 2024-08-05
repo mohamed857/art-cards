609caedbe7b62d8ad90164147fb40516e14269e2
mohamed857 <mohamed857>
2024-08-05 23:26:12 +0300

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { FirstComponent } from "./cards/first/first.component";
+import { Card2Component } from "./cards/card2/card2.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [CommonModule, RouterOutlet, FirstComponent, Card2Component]
 })
 export class AppComponent {
   title = 'cards';
